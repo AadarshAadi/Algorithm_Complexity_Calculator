2fc42cf3577b40d443ffce2abffd3eb2f86e9a99
DELL <mishra1234900@gmail.com>
2024-07-30 12:58:25 +0530

    Added Line differentiator

--- app/src/main/java/com/example/algorithmcomplexitycalculator/complexitymain.java
+++ app/src/main/java/com/example/algorithmcomplexitycalculator/complexitymain.java
@@ -1,9 +1,28 @@
 package com.example.algorithmcomplexitycalculator;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.LinkedList;
+
 public class complexitymain extends AppCompatActivity {
+    EditText algo;
+    Button analyze;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_area);
+        algo=findViewById(R.id.textView3);
+        analyze=findViewById(R.id.button);
+        analyze.setOnClickListener(view -> {
+            LinkedList<String> afg = new LinkedList<>();
+            String[] lines = algo.getText().toString().split("\n");
+
+            for (String line : lines) {
+                afg.push(line);
+            }
+            System.out.println(afg);
+        });
     }
 }
