de753b6e6a701663dc8704bbdb38a01cceb9e0ca
DELL <mishra1234900@gmail.com>
2024-07-30 12:35:36 +0530

    Commitment

--- app/src/main/java/com/example/algorithmcomplexitycalculator/complexitymain.java
+++ app/src/main/java/com/example/algorithmcomplexitycalculator/complexitymain.java
@@ -1,10 +1,9 @@
 package com.example.algorithmcomplexitycalculator;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import androidx.appcompat.app.AppCompatActivity;
 public class complexitymain extends AppCompatActivity {
-    public void onCreate(Bundle savedInstanceState,PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.menu);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_area);
     }
 }
